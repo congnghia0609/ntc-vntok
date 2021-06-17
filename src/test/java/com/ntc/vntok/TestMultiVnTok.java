b17056bed64d1267b16121b2a7811045d0ca3625
nghiatc <congnghia0609@gmail.com>
2021-06-18 01:11:18 +0700

    add unit test

---
+++ src/test/java/com/ntc/vntok/TestMultiVnTok.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ntc.vntok;
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author nghiatc
+ * @since Jun 17, 2021
+ * 
+ * cd ntc-vntok
+ *
+ * # Run all the unit test classes. 
+ * mvn test
+ *
+ * # Run a single test class. 
+ * mvn -Dtest=TestApp1 test
+ *
+ * # Run a single test method from a test class. 
+ * mvn -Dtest=TestApp1#methodname test
+ */
+public class TestMultiVnTok {
+    private static VnTok vntok;
+    private static VnTok vntok2;
+
+    @BeforeClass
+    public static void initTestDefault() throws IOException {
+        VTConfig vtc = new VTConfig();
+        vntok = new VnTok(vtc);
+        vntok2 = new VnTok();
+    }
+
+    @Before
+    public void beforeEachTest() {
+        //System.out.println("This is executed before each Test");
+    }
+
+    @After
+    public void afterEachTest() {
+        //System.out.println("This is executed after each Test");
+    }
+
+    @Test
+    public void testMultiVnTok() {
+        String s = "VNTok là công cụ tách từ Tiếng Việt.";
+        String exp = "VNTok là công_cụ tách từ Tiếng_Việt .";
+        String rs1 = vntok.tokenizeSentence(s);
+        Assert.assertEquals("TestMultiVnTok", exp, rs1);
+        String rs2 = vntok2.tokenizeSentence(s);
+        Assert.assertEquals("TestMultiVnTok2", exp, rs2);
+    }
+}
---
+++ src/test/java/com/ntc/vntok/TestVTConfig.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ntc.vntok;
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author nghiatc
+ * @since Jun 17, 2021
+ * 
+ * cd ntc-vntok
+ *
+ * # Run all the unit test classes. 
+ * mvn test
+ *
+ * # Run a single test class. 
+ * mvn -Dtest=TestApp1 test
+ *
+ * # Run a single test method from a test class. 
+ * mvn -Dtest=TestApp1#methodname test
+ */
+public class TestVTConfig {
+    private static VnTok vntok;
+
+    @BeforeClass
+    public static void initTestDefault() throws IOException {
+        VTConfig vtc = new VTConfig();
+        vntok = new VnTok(vtc);
+    }
+
+    @Before
+    public void beforeEachTest() {
+        //System.out.println("This is executed before each Test");
+    }
+
+    @After
+    public void afterEachTest() {
+        //System.out.println("This is executed after each Test");
+    }
+
+    @Test
+    public void testVTConfig() {
+        String s = "VNTok là công cụ tách từ Tiếng Việt.";
+        String rs = vntok.tokenizeSentence(s);
+        String exp = "VNTok là công_cụ tách từ Tiếng_Việt .";
+        Assert.assertEquals("TestVTConfig", exp, rs);
+    }
+}
